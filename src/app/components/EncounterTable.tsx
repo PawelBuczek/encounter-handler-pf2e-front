5bc4de0d4c09be59bb9bdd714d1352d53ddd09ba
Pawel <pawel.buczek@email.com>
2023-07-13 22:31:56 +0200

    moved EncounterTable to an FC component

---
+++ src/app/components/EncounterTable.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+interface Encounter {
+  name: string;
+  description: string;
+  timeCreated: string;
+}
+
+const EncounterTable: React.FC = () => {
+  const [encounters, setEncounters] = useState<Encounter[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/encounter')
+      .then((response) => response.json())
+      .then((data) => setEncounters(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <main>
+      <div>
+        <h1 className="p-2 pb-4">Encounters:</h1>
+        <ul className="space-y-4">
+          {encounters.map((encounter) => (
+            <li
+              key={encounter.timeCreated}
+              className="bg-gray-800 text-slate-200 p-4 rounded shadow"
+            >
+              <h2 className="text-xl font-bold text-slate-200">{encounter.name}</h2>
+              <p>{encounter.description}</p>
+              <p className="text-sm text-slate-400 italic">
+                Created on: {new Date(encounter.timeCreated).toLocaleString()}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
+};
+
+export default EncounterTable;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,42 +2,15 @@
 
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import React from 'react';
+import EncounterTable from './components/EncounterTable';
 
-interface Encounter {
-  name: string;
-  description: string;
-  timeCreated: string;
-}
 
 export default function Home() {
-  const [encounters, setEncounters] = useState<Encounter[]>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:8080/encounter')
-      .then((response) => response.json())
-      .then((data) => setEncounters(data))
-      .catch((error) => console.log(error));
-  }, []);
 
   return (
     <main>
-      <div>
-        <h1 className="p-2 pb-4">Encounters:</h1>
-        <ul className="space-y-4">
-          {encounters.map((encounter) => (
-            <li
-              key={encounter.timeCreated}
-              className="bg-gray-800 text-slate-200 p-4 rounded shadow"
-            >
-              <h2 className="text-xl font-bold text-slate-200">{encounter.name}</h2>
-              <p>{encounter.description}</p>
-              <p className="text-sm text-slate-400 italic">
-                Created on: {new Date(encounter.timeCreated).toLocaleString()}
-              </p>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <EncounterTable />
     </main>
   );
 }
