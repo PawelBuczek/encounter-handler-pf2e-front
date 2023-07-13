361084aa545f117502a68f6bd13dfab519d4c3eb
Pawel <pawel.buczek@email.com>
2023-07-13 22:55:18 +0200

    upgraded the look of EncounterTable

--- src/app/components/EncounterTable.tsx
+++ src/app/components/EncounterTable.tsx
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import './EncounterTable.css';
 
 interface Encounter {
   name: string;
   description: string;
   timeCreated: string;
+  [key: string]: string; // Add index signature
 }
+
+type SortDirection = 'ascending' | 'descending';
+
+type SortConfig = {
+  field: keyof Encounter;
+  direction: SortDirection;
+};
+
+const defaultSortConfig: SortConfig = {
+  field: 'name',
+  direction: 'ascending',
+};
 
 const EncounterTable: React.FC = () => {
   const [encounters, setEncounters] = useState<Encounter[]>([]);
+  const [sortConfig, setSortConfig] = useState<SortConfig>(defaultSortConfig);
 
   useEffect(() => {
     fetch('http://localhost:8080/encounter')
@@ -16,24 +31,61 @@ const EncounterTable: React.FC = () => {
       .catch((error) => console.log(error));
   }, []);
 
+  const handleSort = (field: keyof Encounter) => {
+    setSortConfig((prevSortConfig) => ({
+      field,
+      direction:
+        prevSortConfig.field === field
+          ? prevSortConfig.direction === 'ascending'
+            ? 'descending'
+            : 'ascending'
+          : 'ascending',
+    }));
+  };
+
+  const sortedEncounters = [...encounters].sort((a, b) => {
+    const { field, direction } = sortConfig;
+    return direction === 'ascending'
+      ? a[field].localeCompare(b[field])
+      : b[field].localeCompare(a[field]);
+  });
+
   return (
     <main>
-      <div>
-        <h1 className="p-2 pb-4">Encounters:</h1>
-        <ul className="space-y-4">
-          {encounters.map((encounter) => (
-            <li
-              key={encounter.timeCreated}
-              className="bg-gray-800 text-slate-200 p-4 rounded shadow"
-            >
-              <h2 className="text-xl font-bold text-slate-200">{encounter.name}</h2>
-              <p>{encounter.description}</p>
-              <p className="text-sm text-slate-400 italic">
-                Created on: {new Date(encounter.timeCreated).toLocaleString()}
-              </p>
-            </li>
-          ))}
-        </ul>
+      <div className="encounter-table-container">
+        <h1 className="table-heading">Encounters</h1>
+        <table className="encounter-table">
+          <thead>
+            <tr>
+              {['name', 'description', 'timeCreated'].map((field) => (
+                <th
+                  key={field}
+                  onClick={() => handleSort(field as keyof Encounter)}
+                >
+                  <span>{field}</span>
+                  {sortConfig.field === field && (
+                    <span className={`sort-icon ${sortConfig.direction}`}>
+                      {sortConfig.direction === 'ascending' ? '▲' : '▼'}
+                    </span>
+                  )}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {sortedEncounters.map((encounter) => (
+              <tr key={encounter.timeCreated}>
+                {['name', 'description', 'timeCreated'].map((field) => (
+                  <td key={field}>
+                    {field === 'timeCreated'
+                      ? new Date(encounter[field]).toLocaleString()
+                      : encounter[field]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </main>
   );
