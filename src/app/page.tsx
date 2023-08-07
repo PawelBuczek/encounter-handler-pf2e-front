2651b0d7fb4f1fc7199ea8003c3b7cf49f58d903
PawelBuczek <pawel.buczek@email.com>
2023-08-08 00:22:26 +0200

    small refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
 import React from 'react';
 import EncounterTable from './components/EncounterTable';
 
