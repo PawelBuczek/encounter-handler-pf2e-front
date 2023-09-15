ff82e1197140863c452b5710ac0a0a1859748a68
PawelBuczek <pawel.buczek@email.com>
2023-09-15 19:52:19 +0200

    moved route.ts to the correct location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 'use client'
+import {options} from "../app/api/auth"
+import {getServerSession} from "next-auth/next"
 
 import React from 'react';
 import EncounterTable from './components/EncounterTable';
 
+async function page(){
+  const session = await getServerSession(options)
+  console.log(session.user)
+}
 
 export default function Home() {
 
@@ -12,3 +18,4 @@ export default function Home() {
     </main>
   );
 }
+
